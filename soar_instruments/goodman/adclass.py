bd6aab0323a11deb5776bc4022ac1f6935a2fa45
Christopher Montalban <chris.montalban@gmail.com>
2024-03-06 16:16:01 -0300

    Change more headers keywords

--- soar_instruments/goodman/adclass.py
+++ soar_instruments/goodman/adclass.py
@@ -40,12 +40,12 @@ class AstroDataGOODMAN(AstroDataSOAR):
 
     @astro_data_tag
     def _tag_image(self):
-        if self.phu.get('CAM_ANG') == 0 and self.phu.get('WAVMOD') == 'IMAGING':
+        if self.phu.get('CAM_TARG') == 0 and self.phu.get('WAVMODE') == 'IMAGING':
             return TagSet(['IMAGE'])
         
     @astro_data_tag
     def _tag_spect(self):
-        if self.phu.get('CAM_ANG') != 0 and self.phu.get('WAVMOD') != 'IMAGING':
+        if self.phu.get('CAM_TARG') != 0 and self.phu.get('WAVMODE') != 'IMAGING':
             return TagSet(['SPECT'])
 
 
